585b8f15a023a50868af7dcfb146df8f9e3db6be
Camilo Carvajal <camilo@talent.com>
2021-10-26 12:00:48 -0500

    composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '038571bc67d62487e5206a5729f45f5d290b826b',
+        'name' => 'cristiancarvajal/questions',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cristiancarvajal/questions' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '038571bc67d62487e5206a5729f45f5d290b826b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
